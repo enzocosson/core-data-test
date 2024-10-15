661fe941dec244b5796565b3f106af355932ccba
enzocosson <enzolemercier@gmail.com>
2024-10-15 13:53:29 +0200

    Initial Commit

---
+++ core-data-test/core_data_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  core_data_testApp.swift
+//  core-data-test
+//
+//  Created by Enzo Cosson on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct core_data_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ core-data-testTests/core_data_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  core_data_testTests.swift
+//  core-data-testTests
+//
+//  Created by Enzo Cosson on 15/10/2024.
+//
+
+import Testing
+@testable import core_data_test
+
+struct core_data_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
